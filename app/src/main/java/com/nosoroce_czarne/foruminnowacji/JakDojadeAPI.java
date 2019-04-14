a01b488df379e2dd0da38c5ed2613b78f3295c98
bartoszsliwa <daszek212@gmail.com>
2019-04-14 19:55:39 +0200

    update

--- app/src/main/java/com/nosoroce_czarne/foruminnowacji/JakDojadeAPI.java
+++ app/src/main/java/com/nosoroce_czarne/foruminnowacji/JakDojadeAPI.java
@@ -33,7 +33,7 @@ public class JakDojadeAPI {
 
         URL url = null;
         HttpURLConnection conn = null;
-        String urlString = "https://jakdojade.pl/tarnow/trasa/z--undefined--do--undefined?tc="+location.getLatitude()+":"+location.getLongitude()+"&fc="+50.0133421/*placeLocation.getLatitude()*/+":"+20.9902551/*placeLocation.getLongitude*/+"&ft=LOCATION_TYPE_COORDINATE&tt=LOCATION_TYPE_COORDINATE&d="+formattedDate+"&h="+formattedTime+"&aro=1&t=1&rc=3&ri=1&r=0";
+        String urlString = "https://jakdojade.pl/tarnow/trasa/z--undefined--do--undefined?tc="+location.getLatitude()+":"+location.getLongitude()+"&fc="+placeLocation.latitude+":"+placeLocation.longitude+"&ft=LOCATION_TYPE_COORDINATE&tt=LOCATION_TYPE_COORDINATE&d="+formattedDate+"&h="+formattedTime+"&aro=1&t=1&rc=3&ri=1&r=0";
         Log.d("URL", urlString);
 
         Intent httpIntent = new Intent(Intent.ACTION_VIEW);

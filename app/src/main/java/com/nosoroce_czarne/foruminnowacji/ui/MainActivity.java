d51332527fcb7898720265279545ce904ddee70e
bwnuk <Bartekwnuk13>
2019-04-14 12:20:12 +0200

    Button works.

---
+++ app/src/main/java/com/nosoroce_czarne/foruminnowacji/ui/MainActivity.java
@@ -0,0 +1,48 @@
+package com.nosoroce_czarne.foruminnowacji.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.nosoroce_czarne.foruminnowacji.R;
+
+public class MainActivity extends AppCompatActivity {
+    private Button events;
+    private Button event;
+    private Button places;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        events = (Button) findViewById(R.id.events);
+        event = (Button) findViewById(R.id.event);
+        places = (Button) findViewById(R.id.places);
+        final Intent intentEvents = new Intent(this, EventsActivity.class);
+        final Intent intentEvent = new Intent(this, SingleActivity.class);
+        final Intent intentPlaces = new Intent(this, PlacesActivity.class);
+
+        events.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intentEvents);
+                finish();            }
+        });
+
+        events.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intentEvent);
+                finish();
+            }
+        });
+
+        events.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intentPlaces);
+                finish();
+            }
+        });
+    }
+}

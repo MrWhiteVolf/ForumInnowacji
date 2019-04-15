0ad5ace69b57cd2cfe6c3850245a8832593e9150
bwnuk <Bartekwnuk13>
2019-04-15 10:32:33 +0200

    Main list

--- app/src/main/java/com/nosoroce_czarne/foruminnowacji/ui/MainActivity.java
+++ app/src/main/java/com/nosoroce_czarne/foruminnowacji/ui/MainActivity.java
@@ -5,25 +5,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.nosoroce_czarne.foruminnowacji.R;
+import com.nosoroce_czarne.foruminnowacji.mockDB.EventDB;
 import com.nosoroce_czarne.foruminnowacji.mockDB.PlaceDB;
+import com.nosoroce_czarne.foruminnowacji.model.Event;
 import com.nosoroce_czarne.foruminnowacji.model.Place;
+import com.nosoroce_czarne.foruminnowacji.util.CustomListAdapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Button events;
     private Button event;
     private Button places;
     private Button single;
-
-
+    private ListView eventList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eventList = (ListView) findViewById(R.id.list_main);
 
+        new EventDB();
+        List<Event> evList = EventDB.getEvents();
+        CustomListAdapter adapter = new CustomListAdapter(this, R.layout.activity_events, evList);
+        eventList.setAdapter(adapter);
     }
 
     public void intentEvents(View view){

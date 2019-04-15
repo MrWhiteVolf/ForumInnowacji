622a8150cd3a87d06181c8f64bdfc4772496cd3f
bwnuk <Bartekwnuk13>
2019-04-15 07:10:44 +0200

    Nowe.

--- app/src/main/java/com/nosoroce_czarne/foruminnowacji/ui/EventsActivity.java
+++ app/src/main/java/com/nosoroce_czarne/foruminnowacji/ui/EventsActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
+import com.nosoroce_czarne.foruminnowacji.mockDB.EventDB;
 import com.nosoroce_czarne.foruminnowacji.util.CustomListAdapter;
 import com.nosoroce_czarne.foruminnowacji.model.Event;
 import com.nosoroce_czarne.foruminnowacji.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EventsActivity extends AppCompatActivity {
     private ListView eventList;
@@ -20,12 +22,7 @@ public class EventsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event_listview);
         eventList = (ListView) findViewById(R.id.eventList);
 
-        ArrayList<Event> evList = new ArrayList<>();
-        evList.add(new Event("drawable://" +R.drawable.hehe, "Bartłomiej Wnuk", "DUAP", "DUPA"));
-        evList.add(new Event("drawable://" +R.drawable.hehe, "Bartłomiej Wnuk", "DUAP", "DUPA"));
-        evList.add(new Event("drawable://" +R.drawable.hehe, "Bartłomiej Wnuk", "DUAP", "DUPA"));
-        evList.add(new Event("drawable://" +R.drawable.hehe, "Bartłomiej Wnuk", "DUAP", "DUPA"));
-        evList.add(new Event("drawable://" +R.drawable.hehe, "Bartłomiej Wnuk", "DUAP", "DUPA"));
+        List<Event> evList = EventDB.getEvents();
 
         CustomListAdapter adapter = new CustomListAdapter(this, R.layout.activity_events, evList);
         eventList.setAdapter(adapter);
